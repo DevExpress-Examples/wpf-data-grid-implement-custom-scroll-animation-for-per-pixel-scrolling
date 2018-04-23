8bd33667cfce117af5eca24ebaec55bcea3eaff8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_CustomScrollAnimation/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace DXGrid_CustomScrollAnimation {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            grid.ItemsSource = GetData();
+        }
+
+        private void tableView1_CustomScrollAnimation(object sender, DevExpress.Xpf.Grid.CustomScrollAnimationEventArgs e) {
+            e.Storyboard = new Storyboard();
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = e.OldOffset;
+            animation.To = e.NewOffset;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(600));
+            animation.EasingFunction = new ExponentialEase() { Exponent = 0 };
+            e.Storyboard.Children.Add(animation);
+        }
+
+        private List<TestDataObject> GetData() {
+            List<TestDataObject> _list = new List<TestDataObject>();
+            for (int i = 0; i < 100; i++)
+                _list.Add(new TestDataObject() { ID = i, Value = string.Format("Value {0}", i)  });
+            return _list;
+        }
+    }
+
+    public class TestDataObject {
+        public int ID { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Implement Custom Scroll Animation for Per-Pixel Scrolling
+
+
+<p>This example shows how to handle the CustomScrollAnimation event to implement custom animation played when grid data is vertically scrolled (per-pixel scrolling). Note the view's AllowPerPixelScrolling and AllowScrollAnimation options must be enabled.</p>
+
+<br/>
+
+
