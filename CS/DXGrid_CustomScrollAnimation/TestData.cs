c29051923a8912f1f4fa2653720547c46c4932ec
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-24 12:02:14 +0400

    Update sample

---
+++ CS/DXGrid_CustomScrollAnimation/TestData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DXGrid_CustomScrollAnimation {
+    public class TestDataObject {
+        public int ID { get; set; }
+        public string Value { get; set; }
+    }
+    public class TestData {
+        public static ObservableCollection<TestDataObject> GetData() {
+            ObservableCollection<TestDataObject> list = new ObservableCollection<TestDataObject>();
+            for (int i = 0; i < 100; i++)
+                list.Add(new TestDataObject() { ID = i, Value = string.Format("Value {0}", i) });
+            return list;
+        }
+    }
+}
