3f85bd0f81e1c60fc21dfe82cc55b421be8050e5
DevExpressExampleBot <fakeEmail>
2023-11-27 17:02:26 +0400

    Source auto update [skip ci]

--- VB/DXGrid_CustomScrollAnimation/MainWindow.xaml.vb
+++ VB/DXGrid_CustomScrollAnimation/MainWindow.xaml.vb
@@ -1,67 +1,46 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports System.Windows.Media.Animation
 
 Namespace DXGrid_CustomScrollAnimation
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = GetData()
-		End Sub
 
-		Private Sub tableView1_CustomScrollAnimation(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomScrollAnimationEventArgs)
-			e.Storyboard = New Storyboard()
-			Dim animation As New DoubleAnimation()
-			animation.From = e.OldOffset
-			animation.To = e.NewOffset
-			animation.Duration = New Duration(TimeSpan.FromMilliseconds(600))
-			animation.EasingFunction = New ExponentialEase() With {.Exponent = 0}
-			e.Storyboard.Children.Add(animation)
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Function GetData() As List(Of TestDataObject)
-			Dim _list As New List(Of TestDataObject)()
-			For i As Integer = 0 To 99
-				_list.Add(New TestDataObject() With {.ID = i, .Value = String.Format("Value {0}", i)})
-			Next i
-			Return _list
-		End Function
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = GetData()
+        End Sub
 
-	Public Class TestDataObject
-		Private privateID As Integer
-		Public Property ID() As Integer
-			Get
-				Return privateID
-			End Get
-			Set(ByVal value As Integer)
-				privateID = value
-			End Set
-		End Property
-		Private privateValue As String
-		Public Property Value() As String
-			Get
-				Return privateValue
-			End Get
-			Set(ByVal value As String)
-				privateValue = value
-			End Set
-		End Property
-	End Class
+        Private Sub tableView1_CustomScrollAnimation(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomScrollAnimationEventArgs)
+            e.Storyboard = New Storyboard()
+            Dim animation As DoubleAnimation = New DoubleAnimation()
+            animation.From = e.OldOffset
+            animation.To = e.NewOffset
+            animation.Duration = New Duration(TimeSpan.FromMilliseconds(600))
+            animation.EasingFunction = New ExponentialEase() With {.Exponent = 0}
+            e.Storyboard.Children.Add(animation)
+        End Sub
+
+        Private Function GetData() As List(Of TestDataObject)
+            Dim _list As List(Of TestDataObject) = New List(Of TestDataObject)()
+            For i As Integer = 0 To 100 - 1
+                _list.Add(New TestDataObject() With {.ID = i, .Value = String.Format("Value {0}", i)})
+            Next
+
+            Return _list
+        End Function
+    End Class
+
+    Public Class TestDataObject
+
+        Public Property ID As Integer
+
+        Public Property Value As String
+    End Class
 End Namespace
